9a33df796fa057b7d67bdfbfa88d7316a6bdabc4
meganii <yuhei24@gmail.com>
2021-09-18 19:19:12 +0900

    Add prefetch

--- themes/tailwindcss/static/sw.js
+++ themes/tailwindcss/static/sw.js
@@ -1,5 +1,8 @@
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init({
+   linkPrefetchOptions: {
+     maxAgeSecondsInCache: 1000
+  } ,
   offlinePageOptions: {
     url: '/offline.html',
     assets: [],
